7a00b44930eed1394b2850baefd1f8967144d38c
Daria Toguleva <73368777+DariaToguleva@users.noreply.github.com>
2026-05-11 12:29:21 -0400

    Time: 2 ms (24.65%) | Memory: 43.3 MB (25.21%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int len = word1.length();
+        if (word1.length() > word2.length()){
+            len = word2.length();
+        }
+
+        String res = "";
+        int i = 0;
+        while (i < len){
+            res += String.valueOf(word1.charAt(i));
+            res += String.valueOf(word2.charAt(i));
+            i++;
+        }
+
+        res += word1.substring(i, i + word1.length() - len);
+        res += word2.substring(i, i + word2.length() - len);
+        return res;
+    }
+}
