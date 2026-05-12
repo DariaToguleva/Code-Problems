7900b5ef41c926be06af20406b7c9d847e2f7639
Daria Toguleva <73368777+DariaToguleva@users.noreply.github.com>
2026-05-12 19:50:31 -0400

    Time: 4 ms (82.16%) | Memory: 44.6 MB (60.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        int[] arr = new int[26];
+        int[] arr2 = new int[26];
+
+        for (Character ch : s.toCharArray()) {
+            arr['z' - ch]++;
+        }
+        for (Character ch : t.toCharArray()) {
+            arr2['z' - ch]++;
         }
 
-        HashMap<Character, Integer> str1 = new HashMap<>();
-        HashMap<Character, Integer> str2 = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            str1.put(s.charAt(i), str1.getOrDefault(s.charAt(i), 0) + 1);
-            str2.put(t.charAt(i), str2.getOrDefault(t.charAt(i), 0) + 1);
-        }
-
-        for (Character key : str1.keySet()) {
-            if (!str2.containsKey(key) || !str1.get(key).equals(str2.get(key))) {
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != arr2[i]) {
                 return false;
             }
         }
